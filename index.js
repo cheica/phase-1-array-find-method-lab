04b9fac9a6b44bc26d7d75ace6a3b86d017ec080
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-22 14:05:30 -0400

    commit tree 91f3fb148253f119e22af917926be4eccccfea01

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// function superbowlWin(record){
+//     if (record.result === 'W'){
+//         return record.year
+//     }
+// }
+
+//record.find(superbowlWin);
+
+
+
+function superbowlWin(record){
+    const findWin = record.find(item => item.result === 'W')
+    if (findWin){
+        return findWin.year
+    }
+    
+    
+}
 
